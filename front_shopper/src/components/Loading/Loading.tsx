791354df1e3f4c486ae7fa5284469df706c971ab
RickHardBR <rickhard@bol.com.br>
2022-10-26 03:03:31 -0300

    Edit styles

--- front_shopper/src/components/Loading/Loading.tsx
+++ front_shopper/src/components/Loading/Loading.tsx
@@ -4,6 +4,9 @@ export default function Loading() {
   return (
     <li className={styles.loading}>
       <p>
+        <div className={styles.indeterminateProgressBar}>
+          <div className={styles.indeterminateProgressBarProgress}></div>
+        </div>
         Loading...
       </p>
     </li>
